dfdf03987c591b95969063c8b7892492e98a04a7
Yensid10 <ben.shearlock@gmail.com>
2026-01-05 12:00:08 +0000

    Added Application entry-point & updated dependencies
    Removed App.java and AppTest.java, added Application.java with SLF4J logging. Updated pom.xml to use property for assertj version and added SLF4J dependency (this was needed as hikari was using an older version of SLF4 compared to logback, so I have now specified what version is needed for logback in the pom). I also adjusted .gitignore to include logs.

---
+++ xtdb-fhir/src/main/java/com/example/Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    logger.info("XTDB FHIR Application starting...");
+  }
+}
