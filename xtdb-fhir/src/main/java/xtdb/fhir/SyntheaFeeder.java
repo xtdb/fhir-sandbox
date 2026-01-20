8c72badabea89591851fc88dfa01f34f8787ed1d
Ernesto Garcia <egg@juxt.pro>
2026-01-20 11:18:18 +0100

    Improved configuration.

--- xtdb-fhir/src/main/java/xtdb/fhir/SyntheaFeeder.java
+++ xtdb-fhir/src/main/java/xtdb/fhir/SyntheaFeeder.java
@@ -1,6 +1,5 @@
 package xtdb.fhir;
 
-import com.example.config.DatabaseConfig;
 import com.example.service.FHIRImportService;
 import com.example.util.JsonUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -9,61 +8,73 @@ import org.mitre.synthea.export.Exporter;
 import org.mitre.synthea.helpers.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
-import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableScheduling
 public class SyntheaFeeder {
   private static final Logger log = LoggerFactory.getLogger(SyntheaFeeder.class);
 
-  @Autowired
-  private SyntheaFeederConf conf;
+  private final DataSource dataSource;
+  private final FHIRImportService importService;
+  private final int population;
 
-  private final Exporter.ExporterRuntimeOptions ero;
-  private final DatabaseConfig dbConf = new DatabaseConfig(new Properties());
-  private final FHIRImportService importService = new FHIRImportService(dbConf.getDataSource());
+  public SyntheaFeeder(DataSource dataSource,
+                       @Value("${synthea-feeder.population:2}") int population) {
+    this.dataSource = dataSource;
+    this.importService = new FHIRImportService(dataSource);
+    this.population = population;
+  }
 
-  public SyntheaFeeder() {
+  @Scheduled(fixedDelayString = "#{${synthea-feeder.interval-seconds} * 1000}")
+  @SuppressWarnings("unused")
+  public void feedPersonRecord() throws InterruptedException {
+    log.debug("new GeneratorOptions...");
+    var options = new Generator.GeneratorOptions();
     Config.set("exporter.fhir.export", "false");
     Config.set("exporter.hospital.fhir.export", "false");
     Config.set("exporter.practitioner.fhir.export", "false");
     Config.set("generate.only_dead_patients", "false");
 
-    Generator.GeneratorOptions options = new Generator.GeneratorOptions();
-    options.population = 1000;
+    options.population = population;
 
-    ero = new Exporter.ExporterRuntimeOptions();
+    log.debug("new ExporterRuntimeOptions...");
+    var ero = new Exporter.ExporterRuntimeOptions();
     ero.enableQueue(Exporter.SupportedFhirVersion.R4);
 
-    // Create and start generator
-    Generator generator = new Generator(options, ero);
-    ExecutorService generatorService = Executors.newFixedThreadPool(1);
-    generatorService.submit(() -> generator.run());
+    log.debug("new Generator...");
+    var generator = new Generator(options, ero);
+    log.debug("starting generator.run thread...");
+    new Thread(() -> {
+      generator.run();
+      log.debug("generator.run done");
+    }).start();
+
+    for (int recordCount = 0; recordCount < options.population; recordCount++) {
+      log.debug("next patient data {} {}...", recordCount, ero.isRecordQueueEmpty());
+      insertRecord(ero.getNextRecord());
+    }
   }
 
-  @Scheduled(fixedRateString = "#{@syntheaFeederConf.intervalSeconds * 1000}")
-  @SuppressWarnings("unused")
-  public void feedPersonRecord() {
+  private void insertRecord(String jsonRecord) {
     try {
-      log.info("obtaining person data...");
-      var personNode = JsonUtil.getMapper().readTree(ero.getNextRecord());
+      var personNode = JsonUtil.getMapper().readTree(jsonRecord);
 
-      log.info("inserting into XTDB...");
-      try (var conn = dbConf.getConnection()) {
+      log.debug("inserting into XTDB...");
+      try (var conn = dataSource.getConnection()) {
         importService.processBundle(personNode, conn);
       }
-      log.info("done");
-    } catch (InterruptedException | JsonProcessingException | SQLException e) {
+
+      log.debug("done");
+    } catch (JsonProcessingException | SQLException e) {
       throw new RuntimeException(e);
     }
   }
